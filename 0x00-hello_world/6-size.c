5b16120f28324d61a499d31036b2a38d7d2a7ee7
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-05 17:47:15 +0100

    create 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
-int intType;
-float floatType;
-double doubleType;
 char charType;
+int intType;
+int longintType;
+int longlongintType;
+float floatType;
 /* sizeof evaluates the size of a variable */
-printf("Size of int: %ld bytes\n", sizeof(intType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
-printf("Size of double: %ld bytes\n", sizeof(doubleType));
 printf("Size of char: %ld byte\n", sizeof(charType));
+printf("Size of int: %ld bytes\n", sizeof(intType));
+printf("Size of longint: %ld bytes\n", sizeof(longintType));
+printf("Size of longlongint: %ld bytes\n", sizeof(longlongintType));
+printf("Size of float: %ld bytes\n", sizeof(floatType));
 return (0);
 }
