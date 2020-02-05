1300f80073ea7c7e654a02f3ff3944ce39eb1dfc
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-05 16:00:22 +0100

    create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char(c)
+printf("with proper grammar, but the outcome is a piece of art,\\n", c);
+return (0);
+}
