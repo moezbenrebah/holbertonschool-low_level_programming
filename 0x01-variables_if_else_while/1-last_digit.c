6b8897beb192f3e231d58ea30d93db1cc80644ee
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-06 17:07:05 +0100

    fix it

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,9 +23,9 @@ else if (n == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
-else
+else  
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastdigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
 return (0);
 }
