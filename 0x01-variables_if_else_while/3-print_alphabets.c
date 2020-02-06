08e1482d299dc7b495597691bff6229d87dcbfae
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-06 18:57:31 +0100

    create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ **/
+int main(void)
+{
+char c, C;
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+while (C <= 'Z')
+{
+putchar(C);
+C++;
+}
+putchar('\n');
+return (0);
+}
