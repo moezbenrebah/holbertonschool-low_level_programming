941f83b6e8c06f94abbdb20d911942da6a173419
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-10 17:21:52 +0100

    create header file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+#ifndef FT_H
+#define    FT_H
+
+void    _putchar(char c);
+void    _putstr(char *str);
+void    _putnbr(int nombre);
+int     _strcmp(char *s1, char *s2);
+int     _strlen(char *str);
+void    _swap(int *a, int *b);
+
+#endif
