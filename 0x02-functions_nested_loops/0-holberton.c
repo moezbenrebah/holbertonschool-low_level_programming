5f38b2a046927316cf54ad2d023180d70858cd2a
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-10 18:55:08 +0100

    fix it

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
  *main - main block
@@ -6,6 +5,15 @@
  */
 int main(void)
 {
-printf("Holberton\n");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
