e54b891e6f734f4bfd87fbdd622ac32b4cd38f0e
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-10 23:12:20 +0100

    fix it

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,10 @@
-#ifndef 
-#define    
+#ifndef HOLBERTON_H
+#define    HOLBERTON_H
 
 void    _putchar(char c);
-
-
+void    print_alphabet(void);
+void    print_alphabet_x10(void);
+int     _islower(int c);
+int     _isalpha(int c);
 
 #endif
