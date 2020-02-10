8442511ce34befe8f7e85a64287f68c6a06d551e
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-10 18:20:23 +0100

    fix it

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,7 +3,7 @@
 
 void    _putchar(char c);
 void    _putstr(char *str);
-void    _putnbr(int nombre);
+void    _putnbr(int number);
 int     _strcmp(char *s1, char *s2);
 int     _strlen(char *str);
 void    _swap(int *a, int *b);
