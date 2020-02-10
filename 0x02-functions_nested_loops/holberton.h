5b2c4030606a33929f4d27d6a7e7224e6ae150a3
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-10 21:33:14 +0100

    fix it

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,10 +2,8 @@
 #define    FT_H
 
 void    _putchar(char c);
-void    _putstr(char *str);
-void    _putnbr(int number);
-int     _strcmp(char *s1, char *s2);
-int     _strlen(char *str);
-void    _swap(int *a, int *b);
-
+void    print_alphabet(void);
+void    print_alphabet_x10(void);
+int     _islower(int c);
 #endif
+
