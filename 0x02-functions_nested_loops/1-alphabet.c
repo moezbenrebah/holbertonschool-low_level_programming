5022c274d6e6c60c6b5523cb54a04190400573d1
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-10 19:12:34 +0100

    fix it

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
  *main - main block
  *Return: 0
  */
-void print_alphabet(void);
-int main(void)
+void print_alphabet(void)
 {
 char c;
 c = 'a';
 while (c <= 'z')
 {
-putchar(c);
+_putchar(c);
 c++;
 }
-putchar('\n');
-return (0);
+_putchar('\n');
 }
