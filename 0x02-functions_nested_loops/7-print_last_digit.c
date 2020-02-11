a77acf7dd4cb71bf74656a66278e849bc3e60677
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-11 11:29:44 +0100

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_last_digit - print the last digit
+ *@n: display last digit
+ *Return: 0;
+ */
+int print_last_digit(int n)
+{
+int last_num(int n);
+if (n >= 0)
+{
+return (n % 10);
+}
+else
+{
+return ((n * -1) % 10);
+}
+}
