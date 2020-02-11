a714f9938f4fc6d3ce904b2eb2c745c47597444a
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-11 16:49:42 +0100

    create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*add - addition operation
+*@num1: first number
+*@num2: seconf number
+*Return: sum
+*/
+
+int add(int num1, int num2)
+{
+int sum;
+
+sum = 0;
+sum = num1 + num2;
+return (sum);
+}
+
