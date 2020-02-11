adf51973549d4c1d8c6814040fc6699ecafec589
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-11 01:20:01 +0100

    create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_sign - display the character sign
+ *@n: print character
+ *Return: 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+return (0);
+}
