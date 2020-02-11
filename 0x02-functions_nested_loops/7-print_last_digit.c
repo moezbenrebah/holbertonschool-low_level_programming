3896678008209f370c7bcd0425b34b1cc1935c3f
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-11 13:43:12 +0100

    check if lastnum is negative

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,9 @@ int print_last_digit(int n)
 {
 	int last_num = 0;
 
-	if (n < 0)
-		n = n * -1;
 	last_num = n % 10;
+	if (n < 0)
+		last_num = last_num * -1;
 	_putchar(last_num + '0');
 	return (last_num);
 }
