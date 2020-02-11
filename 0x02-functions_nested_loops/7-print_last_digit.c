bd9dfb89ede1fb6699a650df8897d96bf064e31f
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-11 12:50:08 +0100

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,20 +6,19 @@
  */
 int print_last_digit(int n)
 {
+int last_num = 0;
 
-	int last_num = 0;
-
-	if (n < 0)
-	{
-		n = n * -1;
-		last_num = n % 10;
-		_putchar('0' + last_num);
-		return (last_num);
-	}
-	else
-	{
-		last_num = n % 10;
-		_putchar('0' + last_num);
-		return (last_num);
-	}
+if (n < 0)
+{
+n = n * -1;
+last_num = n % 10;
+_putchar('0' + last_num);
+return (last_num);
+}
+else
+{
+last_num = n % 10;
+_putchar('0' + last_num);
+return (last_num);
+}
 }
