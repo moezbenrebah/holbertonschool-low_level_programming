dac8a254aff48d62c204967001a2d953d9987570
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-12 12:20:16 +0100

    create 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
-
+/**
+ * main - main function
+ * @void: parameters
+ *Return: 0
+ */
 int main(void)
 {
-  long long int a, b, x;
-  int i;
-  a = 1;
-  b = 2;
+long long int a, b, x;
+int i;
+a = 1;
+b = 2;
 
-  printf("%lld, ", a);
-  printf("%lld, ", b);
+printf("%lld, ", a);
+printf("%lld, ", b);
 
-  for (i = 3; i <= 50; i++)
-    {
-      x = a + b;
-      a = b;
-      b = x;
-      if (i != 50)
-	printf("%lli, ", x);
-      else
-	printf("%lli", x);
-    }
-  printf("\n");
-    }
+for (i = 3; i <= 50; i++)
+{
+x = a + b;
+a = b;
+b = x;
+if (i != 50)
+printf("%lli, ", x);
+else
+printf("%lli", x);
+}
+printf("\n");
+return (0);
+}
