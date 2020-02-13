7d845372e06fe97ce212d0244398cad6c705456f
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-13 22:29:08 +0100

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,19 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
-printf("FizzBuzz ");
+printf("FizzBuzz");
 
 else if ((i % 5) == 0)
-printf("Buzz ");
+printf("Buzz");
 
 else if ((i % 3) == 0)
-printf("Fizz ");
+printf("Fizz");
 
 else
-printf("%d ", i);
+printf("%d" , i);
+
+if (i != 100)
+printf(" ");
 }
 printf("\n");
 return (0);
