6a6a51c3a352ed90fab7aca2833c4456b290ec57
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-13 10:59:36 +0100

    create 4-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers - display digits
+ *@a: integer
+ *Return: no return
+ */
+
+void print_most_numbers(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+  if (a != 2 && a != 4)
+_putchar(a + '0');
+ 
+_putchar('\n');
+}
