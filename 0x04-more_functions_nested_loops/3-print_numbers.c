0162c91ce5370fa346d115a8728f4de46d346a7b
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-13 10:49:48 +0100

    create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - print numbers
+ *Return: no 0
+ */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+_putchar(a + '0');
+_putchar('\n');
+}
