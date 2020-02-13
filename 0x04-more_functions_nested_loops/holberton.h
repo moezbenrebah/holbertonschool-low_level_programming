303016d075a922230daa2cc565eebae5d359e210
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-13 14:00:56 +0100

    update

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -7,3 +8,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(char c);
+void print_numbers(void);
