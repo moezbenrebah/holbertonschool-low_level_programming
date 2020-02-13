9c5dc77695da0d3c449537d48f31fe3ba4dd0b34
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-13 14:11:04 +0100

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  *print_most_numbers - display digits
- *@a: integer
  *Return: no return
  */
 
@@ -11,8 +10,9 @@ void print_most_numbers(void)
 int a;
 
 for (a = 0; a <= 9; a++)
-  if (a != 2 && a != 4)
+
+if (a != 2 && a != 4)
 _putchar(a + '0');
- 
+
 _putchar('\n');
 }
