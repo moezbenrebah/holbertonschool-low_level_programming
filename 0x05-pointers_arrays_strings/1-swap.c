0ac92b71a2756cef9454729176dde7709513dd50
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-17 15:45:09 +0100

    create swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *swap_int - swap numbers function
+ *@a: integer
+ *@b: integer
+ *
+ *Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
