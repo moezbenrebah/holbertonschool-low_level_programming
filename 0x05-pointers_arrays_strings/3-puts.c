46106b52c331092546bb7d2f0ff09d2f8863952e
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-18 14:13:01 +0100

    print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,10 +11,10 @@ void _puts(char *str)
 {
 int i = 0;
 
- while (str[i] != '\0')
-   {
-   _putchar(str[i]);
-   i++;
-   }
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
 _putchar('\n');
 }
