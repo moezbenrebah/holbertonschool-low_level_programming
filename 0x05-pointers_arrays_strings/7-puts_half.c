604eeb346499a632f2de1c27a39ea1c8643fa690
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-19 19:17:54 +0100

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *puts_half - print half string
+ *@str: string
+ *
+ *Return: nothing
+ */
+
+void puts_half(char *str)
+{
+int i = 0;
+int j = 0;
+
+while (str[i] != '\0')
+{
+i++;
+}
+for (j = 0; str[j] != '\0'; j++)
+{
+if (j >= i / 2)
+_putchar(str[j]);
+}
+_putchar('\n');
+}
