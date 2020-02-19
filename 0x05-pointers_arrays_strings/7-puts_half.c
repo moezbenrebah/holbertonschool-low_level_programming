c2606d78af5e477ae6bb9c74b12b72d0f8b192a3
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-20 00:36:25 +0100

    fix it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,10 +17,21 @@ while (str[i] != '\0')
 {
 i++;
 }
+if (i % 2 == 0)
+{
+for (j = 0; str[j] != '\0'; j++)
+{
+if (j >= i / 2)
+_putchar(str[j]);
+}
+}
+else
+{
 for (j = 0; str[j] != '\0'; j++)
 {
 if (j > i / 2)
 _putchar(str[j]);
+}
 }
 _putchar('\n');
 }
