97fe376e00df94e7bc27c0c9340dc9137ddf67d2
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-21 13:23:18 +0100

    encode strings

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,3 +9,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
+char *leet(char *);
