c5c0bad57c80f206c7ba3a340e24510ee0457299
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-24 22:57:47 +0100

    locate a character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_strchr - locates character in a string
+ *@s: string
+ *@c: character
+ *
+ *Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+
+if (*(s + i) == c)
+return (s + i);
+
+return (0);
+}
