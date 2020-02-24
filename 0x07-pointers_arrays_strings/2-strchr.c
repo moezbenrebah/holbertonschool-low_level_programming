20cb0cb83ec2d551e146a1d31404ea9beee9ebbc
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-24 23:27:53 +0100

    locate a character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,14 @@ char *_strchr(char *s, char c)
 {
 int i = 0;
 
-for (i = 0; s[i] != '\0'; i++)
+while (s[i] != '\0')
+i++;
 
+for (i = 0; *(s + i); i++)
+{
 if (*(s + i) == c)
 return (s + i);
-
+i++;
+}
 return (0);
 }
