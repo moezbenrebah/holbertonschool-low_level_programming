f389b985326ec6d712f441f10cc8a566e95bf3b3
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-24 21:43:02 +0100

    copy memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_memcpy - copies bytes from memory area src to memory area dest
+ *@dest: destination area memory
+ *@src: source memory area
+ *@n: data
+ *
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+i = 0;
+
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
