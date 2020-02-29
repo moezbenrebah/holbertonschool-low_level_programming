4164beee7e3b84603ab0ec5a982940ce7e727479
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-02-29 09:11:10 +0100

    static & dynamic libraries

---
+++ 0x09-static_libraries/islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <ctype.h>
+/**
+ *_islower - check for lowercase alphabet
+ *@c: the character to print
+ *Return: 0
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+return (0);
+}
