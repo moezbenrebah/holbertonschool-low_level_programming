a41cd88192b9fff2278be443246e1c53e35216f6
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-01 14:11:06 +0100

    one argument per line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: count of the arguments supplied to the program
+ *@argv: array of pointers to the strings
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+
+return (0);
+}
