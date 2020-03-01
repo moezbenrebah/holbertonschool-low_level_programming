17621f31f584905cfdde3d366b3c6cf0a763cbdb
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-01 13:50:06 +0100

    arguments number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: count of the arguments supplied to the program
+ *@argv: array of pointers to the strings
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}
+
