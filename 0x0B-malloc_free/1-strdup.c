ad5f222e86ddb0f846195edb5c45a19e6d11f7bf
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-03 20:42:53 +0100

    duplicate a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_strdup - duplicate a string
+ *@str: string
+ *
+ *Return: pointer to newely allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+char *str1;
+int i, j;
+
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; i <= *str; i++)
+{
+;
+}
+i++;
+str1 = malloc(strlen(str) + 1);
+for (j = 0; j < i; j++)
+str1[j] = str[j];
+if (str1 == NULL)
+{
+return (NULL);
+}
+return (str1);
+}
