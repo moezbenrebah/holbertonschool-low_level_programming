dba2ffaf5d9decd1c57c8605321aebf7ebab92fb
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-05 18:13:54 +0100

    allocate memory for an array

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,5 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+
+#endif
