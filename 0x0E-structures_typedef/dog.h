08c07b70f8e6fdd65fe20f6cf3aea9e1009f501a
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-09 19:23:37 +0100

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *struct dog - dog description
+ *@name: it's name
+ *@age: it's age
+ *@owner: it's owner
+ *
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
