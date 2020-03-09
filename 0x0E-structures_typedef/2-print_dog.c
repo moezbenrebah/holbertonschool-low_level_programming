548e0a050eb3e9b7a067fe276c01cc0e5b2412fa
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-09 22:35:53 +0100

    fix it

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ if (d == NULL)
 return;
 if (d->name == NULL)
 printf("Name: (nil)\n");
-else if (d->owner == NULL)
+if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
