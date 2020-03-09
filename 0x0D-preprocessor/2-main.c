bb62730b85e624cb4aa4303a7cdc0f235ffa0399
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-09 15:53:04 +0100

    print the filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
--- 0x0D-preprocessor/holberton.h
+++ 0x0D-preprocessor/holberton.h
@@ -1,1 +1,2 @@
 int _putchar(char c);
+int main (void);
