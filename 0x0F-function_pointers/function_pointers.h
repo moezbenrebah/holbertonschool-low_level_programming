466f72fbf20930688916c3d844bf9f6896d5c401
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-11 12:38:19 +0100

    first commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
+#ifndef _FUNCTION_H
+#define _FUNCTION_H
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+#endif
