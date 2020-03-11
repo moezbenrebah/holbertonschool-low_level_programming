ceb808ec1354c47295704731849694830b7da31b
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-11 11:07:47 +0100

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - print the name
+ *@name: string
+ *@f: function to print with
+ *
+ *Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+The function pointer project
