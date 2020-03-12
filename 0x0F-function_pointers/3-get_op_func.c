c61db38b6396981da6ad54b6186bad8c02b6627f
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-12 18:40:08 +0100

    performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ *get_op_func - selects the operation to perform
+ *@s: operation to perform
+ *
+ *Return: pointer to the operation function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+
+int i = 0;
+
+while (ops[i].op != NULL)
+{
+if (*s == *(ops[i].op))
+return (ops[i].f);
+i++;
+}
+printf("Error\n");
+exit(99);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+
+/**
+ *main - performs simple operations
+ *@argc: number of arguments passed
+ *@argv: array of pointers to arguments
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+int (*ptr)(int, int);
+int n1, n2, n3;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+n1 = atoi(argv[1]);
+n2 = atoi(argv[3]);
+
+if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+{
+printf("Error\n");
+exit(99);
+}
+ptr = get_op_func(argv[2]);
+n3 = ptr(n1, n2);
+
+printf("%d\n", n3);
+return (0);
+}
