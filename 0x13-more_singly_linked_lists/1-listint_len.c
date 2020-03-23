2fdbfa4fa0f8d2c59662ead19f313efea26fdff5
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-23 13:43:39 +0100

    number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *listint_len - function to return number of elements
+ *@h: pointer to head
+ *
+ *Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+const listint_t *tmp;
+int i = 0;
+
+tmp = h;
+
+while (tmp != NULL)
+{
+if (tmp->n)
+tmp = tmp->next;
+i++;
+}
+return (i);
+}
