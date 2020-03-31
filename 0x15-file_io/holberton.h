1653b5df5f3c24a781f503e3ff14f20407f26f4b
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-31 19:48:08 +0100

    first commit

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,10 +2,12 @@
 #define _FILE_H_
 
 
+#include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
