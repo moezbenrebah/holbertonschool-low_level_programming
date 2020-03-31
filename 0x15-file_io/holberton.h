d7588aa0a144f950d62c55c2620b2edcb9c0f84f
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-31 19:38:48 +0100

    first commit

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,10 +8,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 #endif
