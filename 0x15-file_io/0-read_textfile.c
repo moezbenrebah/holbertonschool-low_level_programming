3147775230079a2228e5284a73b00e89a13c60ab
Moez Ben Rebah <benrebah.moez@gmail.com>
2020-03-31 19:44:54 +0100

    fix it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  *Return: number of letters
  */
 
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 
 int fd, fd1;
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -3,7 +3,6 @@
 
 
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
