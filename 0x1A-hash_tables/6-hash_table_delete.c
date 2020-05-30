0ef60ed144bf8dd34c4fd8c09d8bcfdeae04db43
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-30 11:07:51 +0100

    delete a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_delete - function to delete a hash table
+ *@ht: a pointer to hash table
+ * 
+ *Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *next, *prev;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		next = ht->array[i];
+		while (next)
+		{
+			prev = next;
+			next = next->next;
+			free(prev->key);
+			free(prev->value);
+			free(prev);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
