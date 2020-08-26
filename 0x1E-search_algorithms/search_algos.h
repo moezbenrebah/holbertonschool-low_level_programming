432ec5d55b5d300526c541f54b4d3c00f319f56d
Boul3ez85 <benrebah.moez@gmail.com>
2020-08-26 15:41:15 +0100

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,7 +10,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-
+int interpolation_search(int *array, size_t size, int value);
 
 
 #endif /* _SEARCH_ALGORITHMS_ */
