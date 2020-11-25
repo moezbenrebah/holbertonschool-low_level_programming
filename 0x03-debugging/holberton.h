3327fb92830b26964f6f030c0590bae386b87086
Moez <57503360+Boul3ez85@users.noreply.github.com>
2020-11-25 03:12:59 +0100

    fix code

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,13 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdio.h>
-void    _putchar(char c);
-void    print_alphabet(void);
-void    print_alphabet_x10(void);
-int     _islower(int c);
-int     _isalpha(int c);
-int     print_sign(int n);
-int     _abs(int);
-int     print_last_digit(int);
-void    jack_bauer(void);
-int     add(int, int);
-void    times_table(void);
-void    print_to_98(int n);
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* HOLBERTON_H */
