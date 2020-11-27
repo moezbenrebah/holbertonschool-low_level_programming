7c974d66762432d35bc2744760f87c8f881590fe
Moez <57503360+Boul3ez85@users.noreply.github.com>
2020-11-27 20:36:10 +0100

    update header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,3 +10,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
